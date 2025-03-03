0a75128e7833e8d927efd9a31cef42f6e367a0cc
aleixop <aleix.obiol@gmail.com>
2025-03-03 10:52:32 +0100

    added conda way

--- scripts/clean_amplicon_datasets.sh
+++ scripts/clean_amplicon_datasets.sh
@@ -6,11 +6,20 @@
 #SBATCH --output=clean-amplicons_%J.out
 #SBATCH --error=clean-amplicons_%J.err
 
+## Load modules if you did not use conda for installation
+
 module load mafft
 module load hmmer
 module load blast
 module load seqkit
 module load R
 module load snakemake
+
+## Activate the environment if you used conda for installation
+
+# module load miniconda # you may need to change this for your cluster
+# conda activate clean_amplicon_datasets
+
+## Run the pipeline
 
 snakemake --cores ${SLURM_CPUS_PER_TASK}
